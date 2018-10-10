28f4ae0f77b21eb08a3336291fd8e0e8dabd1465
Sandra Chen <scchen17@gmail.com>
2018-10-09 18:09:58 -0700

    initial entries complete work test done

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -87,11 +87,13 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-         beforeEach(function() {
-            loadFeed(0);
+         beforeEach(function(done) {
+            loadFeed(0, done);
          });
+         
          it('complete work', function(){
-            
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length > 0).toBe(true);
          });
     });
         
